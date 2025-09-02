2495cbe2cecc95018d5b3c53146df67e3013e674
RenanGroh <renangroh@gmail.com>
2025-09-02 10:57:53 -0300

    feat: Execicio 2

--- Variaveis/Exercicios_pg36.c
+++ Variaveis/Exercicios_pg36.c
@@ -13,5 +13,16 @@ int main () {
     soma = a + b + c + d;
     printf("A soma dos numeros e: %d\n", soma);
 
+    // Exercicio 2.
+    // Faça um programa que receba três notas,
+    // calcule e mostre a média aritmética.
+
+    printf(" #### Exercicio 2: ####\n");
+    float nota1, nota2, nota3, media;
+    printf("Digite tres notas: ");
+    scanf("%f %f %f", &nota1, &nota2, &nota3);
+    media = (nota1 + nota2 + nota3) / 3;
+    printf("A media das notas e: %.2f\n", media);
+
     
 }
