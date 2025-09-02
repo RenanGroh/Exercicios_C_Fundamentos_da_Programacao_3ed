736526c087c9c1d3e8bdc139981765147d73ad23
RenanGroh <renangroh@gmail.com>
2025-09-02 11:52:05 -0300

    feat: Exercicio 18 pg 51

---
+++ Variaveis/Exercicios_pg51.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Exercícios da pagina 51
+
+int main () {
+
+    // Exercício 18
+    // Faça um programa que receba uma temperatura em Celsius, calcule e mostre essa temperatura em
+    // Fahrenheit. Sabe-se que F = 180*(C + 32)/100.
+
+    printf("#### Exercicio 18 ####\n");
+
+    float celsius;
+    printf("Digite a temperatura em Celsius: ");
+    scanf("%f", &celsius);
+
+    float fahrenheit = 180 * (celsius + 32) / 100;
+    printf("A temperatura em Fahrenheit e: %.2f\n", fahrenheit);
+
+    return 0;
+}
