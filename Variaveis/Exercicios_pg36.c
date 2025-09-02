e37db8fc357579b3acd6e9893e6dfddc163d451a
RenanGroh <renangroh@gmail.com>
2025-09-02 10:50:34 -0300

    feat: Execicio 1

---
+++ Variaveis/Exercicios_pg36.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+    // Exercicio 1.
+    // Faça um programa que receba quatro números inteiros, calcule 
+    // e mostre a soma desses números.
+
+    printf(" #### Exercicio 1: ####\n");
+    int a, b, c, d, soma;
+    printf("Digite quatro numeros inteiros: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    soma = a + b + c + d;
+    printf("A soma dos numeros e: %d\n", soma);
+
+    
+}
