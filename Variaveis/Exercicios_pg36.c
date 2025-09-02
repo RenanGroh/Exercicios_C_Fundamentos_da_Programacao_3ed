4d93b0e55bd829f256e3fb727056f173f62d9209
RenanGroh <renangroh@gmail.com>
2025-09-02 11:04:23 -0300

    feat: Execicio 2

--- Variaveis/Exercicios_pg36.c
+++ Variaveis/Exercicios_pg36.c
@@ -24,5 +24,15 @@ int main () {
     media = (nota1 + nota2 + nota3) / 3;
     printf("A media das notas e: %.2f\n", media);
 
+    // Exercicio 3.
+    // Faça um programa que receba três notas e seus 
+    // respectivos pesos,calcule e mostre a média ponderada.
+
+    printf(" #### Exercicio 3: ####\n");
+    float nota1, nota2, nota3, peso1, peso2, peso3, media_ponderada;
+    printf("Digite tres notas e seus respectivos pesos: ");
+    scanf("%f %f %f %f %f %f", &nota1, &peso1, &nota2, &peso2, &nota3, &peso3);
+    media_ponderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+    printf("A media ponderada e: %.2f\n", media_ponderada);
     
 }
